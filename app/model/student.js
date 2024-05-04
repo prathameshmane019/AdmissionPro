50e719dab6eda1c0f16e0d72cc236e9c577da295
12DeshmukhAish <113546938+12DeshmukhAish@users.noreply.github.com>
2024-05-04 21:56:57 +0530

    schema added of student

---
+++ app/model/student.js
@@ -0,0 +1,65 @@
+import { Double } from 'mongodb';
+import mongoose from 'mongoose';
+
+const { Schema, model } = mongoose;
+
+const studentSchema = new Schema(
+  {
+    Name: {
+      type: String,
+      required: true,
+    },
+    FName: {
+        type: String,
+       
+      },
+      LName: {
+        type: String,
+       
+      },
+      MName: {
+        type: String,
+       
+      },
+     Gender:{
+        type:String,
+     },
+     DOB:{
+        type:String,
+     },
+     Category:{
+        type:String,
+     },
+     Disability:{
+        type:String,
+     },
+     HSC:{
+        type: Double,
+     },
+     SSC:{
+        type: Double,
+     },
+     CET:{
+        type: Double,
+     },
+     JEE:{
+        type: Double,
+     },
+   
+    
+    
+    pwd: {
+      type: String,
+      
+    },
+    
+   
+  },
+  {
+    timestamps: true, // Automatically add createdAt and updatedAt fields
+  }
+);
+
+const Student = mongoose.models.Student || model('Student', studentSchema);
+
+export default Student;

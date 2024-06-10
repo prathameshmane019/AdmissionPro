cdf260ab4dcf03c59d1130bcad6ca63bf840282d
12DeshmukhAish <113546938+12DeshmukhAish@users.noreply.github.com>
2024-06-10 10:07:14 +0530

    home page designed

--- app/(main)/faculty/page.js
+++ app/(main)/faculty/page.js
@@ -92,31 +92,7 @@ function Dashboard() {
   return (
     <div className="container ml-4">
       <h1 className="text-3xl text-center my-8">Dashboard</h1>
-      <div className="grid grid-cols-2 md:grid-cols-2 lg:grid-cols-3 gap-4">
-        <Card onClick={() => router.push("/cluster")} shadow="md" className="max-w-[350px] rounded-lg overflow-hidden bg-gradient-to-br from-blue-500 to-blue-700 hover:from-blue-600 hover:to-blue-800 transform hover:scale-105 transition duration-300 ease-in-out">
-          <CardBody className="text-center">
-            <UserCircleIcon className="w-16 h-16 text-white mx-auto mb-4" />
-            <h5 className="mb-2 text-white">Total Clusters</h5>
-            <h2 className="text-3xl font-bold text-white">{data.clusters}</h2>
-          </CardBody>
-        </Card>
-
-        <Card onClick={() => router.push("/faculty")} shadow="md" className="max-w-[350px] rounded-lg overflow-hidden bg-gradient-to-br from-green-500 to-green-700 hover:from-green-600 hover:to-green-800 transform hover:scale-105 transition duration-300 ease-in-out">
-          <CardBody className="text-center">
-            <UsersIcon className="w-16 h-16 text-white mx-auto mb-4" />
-            <h5 className="mb-2 text-white">Total Faculty</h5>
-            <h2 className="text-3xl font-bold text-white">{data.facultyCount}</h2>
-          </CardBody>
-        </Card>
-
-        <Card onClick={() => router.push("/students")} shadow="md" className="max-w-[350px] rounded-lg overflow-hidden bg-gradient-to-br from-yellow-500 to-yellow-700 hover:from-yellow-600 hover:to-yellow-800 transform hover:scale-105 transition duration-300 ease-in-out">
-          <CardBody className="text-center">
-            <UserGroupIcon className="w-16 h-16 text-white mx-auto mb-4" />
-            <h5 className="mb-2 text-white">Total Students</h5>
-            <h2 className="text-3xl font-bold text-white">{data.studentCount}</h2>
-          </CardBody>
-        </Card>
-      </div>
+      
 
       <div className="mt-8">
         <h2 className="text-2xl text-center mb-4">Interested Branch Statistics</h2>
